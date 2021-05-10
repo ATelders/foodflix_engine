a1ef49b023a73834802e85b0b54d492373803596
Arthur Telders <atelders@gmail.com>
2021-05-10 17:07:30 +0200

    Update

--- src/main.py
+++ src/main.py
@@ -28,32 +28,42 @@ def load_data(path):
     return listings
 
 @st.cache(allow_output_mutation=True)
-def create_tfidf_matrix(input):
-    tf = TfidfVectorizer(analyzer = 'word', ngram_range = (1, 2), min_df = 0, stop_words = STOPWORDSFR)
-    tfidf_matrix = tf.fit_transform(input)
+def create_model(data, model):
+    if model == 'TfidfVectorizer':
+        tf = TfidfVectorizer(analyzer = 'word', ngram_range = (1, 2), min_df = 0, stop_words = STOPWORDSFR)
+    elif model == 'CountVectorizer':
+        tf = CountVectorizer(analyzer = 'word', ngram_range = (1, 2), min_df = 0, stop_words = STOPWORDSFR)
+    else:
+        tf = TfidfVectorizer(analyzer = 'word', ngram_range = (1, 2), min_df = 0, stop_words = STOPWORDSFR)     
+    tfidf_matrix = tf.fit_transform(data)
     return tf, tfidf_matrix
 
-def get_similar_products(input):
+def get_similar_products(input, slider_input=5):
     user_matrix = tf.transform([input])
     cosine_similarities = linear_kernel(user_matrix, tfidf_matrix)
     similar_indices = cosine_similarities[0].argsort()[:-100:-1]
     similar_items = [(cosine_similarities[0][i], listings['id'][i]) for i in similar_indices]
     results = []
-    for i in range(10):
+    for i in range(slider_input):
         results.append(listings.iloc[similar_items[i][1]])
     return results
 
 
 
 if __name__ == '__main__':
+    st.title('Foodflix')
+    st.text('Source des données : OpenFoodFacts.org')
+
+    slider_input = st.sidebar.slider('Nombre de résultats', min_value=5, max_value=20)
+    model_input = st.sidebar.radio('Choix du modèle NLP', ['TfidfVectorizer','CountVectorizer','BERT'])
+
     listings = load_data(DATA_PATH)
     CONTENT = listings['content']
     STOPWORDSFR = {'le', 'la', 'de', 'du', 'au', 'les', 'aux', 'null'}
-    tf, tfidf_matrix = create_tfidf_matrix(CONTENT)
-    st.title('Foodflix')
+    tf, tfidf_matrix = create_model(CONTENT, model_input)
     user_input = st.text_input("Chercher un produit", value='', max_chars=60, key=None, type='default', help=None)
     if user_input:
-        results = get_similar_products(user_input)
+        results = get_similar_products(user_input, slider_input)
         for item in results:
             st.title('Marque: {}'.format(item['brands']))
             st.write('Nom du produit: {}'.format(item['product_name']))
